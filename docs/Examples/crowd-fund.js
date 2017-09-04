67378c03157ad28843e5cdca092175bff06227d5
Buck Perley <bucko.perley@gmail.com>
2017-09-04 16:16:38 -0700

    notes for steps

--- docs/Examples/crowd-fund.js
+++ docs/Examples/crowd-fund.js
@@ -9,10 +9,15 @@ const fundingTarget = 100000000; // 1 BTC
 // Step 1: Create a blank mtx
 const fundMe = new MTX();
 
-// Step 2: Add an output with the target amount you would like to raise and an address you control
-const fundeeWallet = await new bcoin.http.Wallet({ id: 'foo', network: 'testnet'});
+// Step 2:
+// Add an output with the target amount
+// you would like to raise and an address you control
+const fundeeWallet = await new bcoin.http.Wallet({
+    id: 'foo',
+    network: 'testnet'
+  });
 const fundeeAddress = await fundeeWallet.createAddress('default');
-
+console.log(fundeeWallet);
 // need this because can't serialize and output mtx with no input
 fundMe.addInput(new bcoin.primitives.Input());
 
@@ -25,9 +30,11 @@ fundMe.addOutput({value: fundingTarget, address: fundeeAddress.address });
 console.log('transaction: ', fundMe);
 })();
 
-
-/*******
+/** *****
 NOTES:
-- need to account for change addresses
-- need to account for who pays the fee (probably should be the receiver)
-*******/
+- need to account for change addresses:
+    - only "exact" UTXOs added, so process for in-exact would be
+      to split a UTXO first sending to yourself
+- need to account for who pays the fee
+  - maybe the receiver adds one final input that is entirely for fee
+** *****/
